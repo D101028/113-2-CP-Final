440f0fba1a552b261648a190637a14e5fcab2211
D101028 <104710811+D101028@users.noreply.github.com>
2025-05-12 14:14:45 +0800

    more new

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import numpy as np
-epsilon = 0.9
-delta = 0.01
+epsilon = 0.1
+delta = 0.001
 rank = 500 # k
-m = int((rank * np.log(42/epsilon) + np.log(2/delta))/(epsilon**2/2))  # Compression dimension (must be > k)
+m = int((rank * np.log(42/epsilon) + np.log(2/delta))/(100000*epsilon**2/2))  # Compression dimension (must be > k)
 print(m)
