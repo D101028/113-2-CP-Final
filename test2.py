6105538790888c3a31ba4ef6c2f2622e2ac223a9
D101028 <104710811+D101028@users.noreply.github.com>
2025-05-19 15:42:31 +0800

    remove some files

--- test2.py
+++ test2.py
@@ -1,6 +1,13 @@
 import numpy as np
-epsilon = 0.1
-delta = 0.001
-k = 10          # rank
-m = int((k * np.log(42/epsilon) + np.log(2/delta))/(1000*epsilon**2/2))  # Compression dimension (must be > k)
-print(m)
+from numpy.linalg import svd 
+
+A = np.array([
+    [1,2],
+    [8,7]
+])
+
+a, b, c  = svd(A)
+
+print(a, b, c)
+
+print()
