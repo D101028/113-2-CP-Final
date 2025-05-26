29a40b9be052d2030ecb41312b593ccaade9faf7
D101028 <104710811+D101028@users.noreply.github.com>
2025-05-26 15:14:30 +0800

    Merge some functions

--- exp_vector_error.py
+++ exp_vector_error.py
@@ -3,21 +3,7 @@ from numpy.linalg import norm
 
 from func import generate_matrix_with_singular_values, sketched_svd, m_func, draw_diagram2
 
-def test_vector_error_JL(N = 500, n = 20, k = 10, epsilon = 0.1, delta = 0.01):
-    ######################################################################
-    ### Danger! Running This Function May Be Very Resources Consuming! ###
-    ######################################################################
-    """
-    Compute results from given epsilon and delta. 
-    m is computed from epsilon and delta. 
-    >>> N, n = 500, 20     # Dimension of X
-    >>> k = 10             # rank
-    >>> epsilon = 0.1      # Error
-    >>> delta = 0.01       # Failure probability
-    """
-    # Compute m
-    m = m_func(k, epsilon, delta)
-
+def compute_errors_and_bounds(N, n, k, m, epsilon):
     # Generate the experiment data
     X, S_X, _, V_X = generate_matrix_with_singular_values(N, n, sigma=np.linspace(10, 1, k))
     _, V_Y = sketched_svd(X, m)
@@ -54,6 +40,24 @@ def test_vector_error_JL(N = 500, n = 20, k = 10, epsilon = 0.1, delta = 0.01):
 
         bound = min(np.sqrt(2), numerator * max_term)
         bounds.append(bound)
+    return errors, bounds
+
+def test_vector_error_JL(N = 500, n = 20, k = 10, epsilon = 0.1, delta = 0.01):
+    ######################################################################
+    ### Danger! Running This Function May Be Very Resources Consuming! ###
+    ######################################################################
+    """
+    Compute results from given epsilon and delta. 
+    m is computed from epsilon and delta. 
+    >>> N, n = 500, 20     # Dimension of X
+    >>> k = 10             # rank
+    >>> epsilon = 0.1      # Error
+    >>> delta = 0.01       # Failure probability
+    """
+    # Compute m
+    m = m_func(k, epsilon, delta)
+
+    errors, bounds = compute_errors_and_bounds(N, n, k, m, epsilon)
 
     # Draw the diagram
     draw_diagram2(
@@ -74,42 +78,8 @@ def test_vector_error_n_JL(N = 500, n = 20, k = 10, epsilon = 0.1, m = 40):
     >>> m = 40            # Set m
     >>> epsilon = 0.1     # Error
     """
-    # Generate the experiment data
-    X, S_X, _, V_X = generate_matrix_with_singular_values(N, n, sigma=np.linspace(10, 1, k))
-    _, V_Y = sketched_svd(X, m)
-
-    # Error(L2) and thereotical bound
-    errors = []
-    bounds = []
-
-    for j in range(k):
-        v = V_X[:, j]
-        v_p = V_Y[:, j]
-        
-        # Ensure the nonnegative inner product
-        if np.dot(v, v_p) < 0:
-            v_p = -v_p
-
-        err = norm(v - v_p)
-        errors.append(err)
-
-        # Thereotical bound
-        sigma_j = S_X[j]
-        numerator = epsilon * np.sqrt(1 + epsilon) / np.sqrt(1 - epsilon)
-        max_term = 0
-        
-        for i in range(k):
-            if i == j:
-                continue
-            sigma_i = S_X[i]
-            denom_candidates = [abs(sigma_i**2 - sigma_j**2 * (1 + c * epsilon)) for c in [-1, 0, 1]]
-            denom = min(denom_candidates)
-            if denom != 0:
-                ratio = np.sqrt(2) * sigma_i * sigma_j / denom
-                max_term = max(max_term, ratio)
-
-        bound = min(np.sqrt(2), numerator * max_term)
-        bounds.append(bound)
+    
+    errors, bounds = compute_errors_and_bounds(N, n, k, m, epsilon)
 
     # Draw the diagram
     draw_diagram2(
--- exp_vector_ratio.py
+++ exp_vector_ratio.py
@@ -2,6 +2,22 @@ import numpy as np
 
 from func import generate_matrix_with_singular_values, sketched_svd, m_func, draw_diagram3
 
+def compute_ratio_and_bounds(N, n, k, m, epsilon):
+    # Generate the experiment data
+    X, true_singular_values, _, _ = generate_matrix_with_singular_values(N, n, sigma=np.linspace(10, 1, k))
+    sketch_singular_values, _ = sketched_svd(X, m)
+
+    # Take the first k terms
+    sigma_X = true_singular_values[:k]
+    sigma_Y = sketch_singular_values[:k]
+
+    # Compute the ratio with the corresponding bound
+    ratios = sigma_Y / sigma_X
+    lower_bound = np.sqrt(1 - epsilon)
+    upper_bound = np.sqrt(1 + epsilon)
+
+    return ratios, lower_bound, upper_bound
+
 def test_value_ratio_JL(N = 500, n = 20, k = 10, epsilon = 0.1, delta = 0.01):
     ######################################################################
     ### Danger! Running This Function May Be Very Resources Consuming! ###
@@ -17,18 +33,7 @@ def test_value_ratio_JL(N = 500, n = 20, k = 10, epsilon = 0.1, delta = 0.01):
     # Compute m
     m = m_func(k, epsilon, delta)
 
-    # Generate the experiment data
-    X, true_singular_values, _, _ = generate_matrix_with_singular_values(N, n, sigma=np.linspace(10, 1, k))
-    sketch_singular_values, _ = sketched_svd(X, m)
-
-    # Take the first k terms
-    sigma_X = true_singular_values[:k]
-    sigma_Y = sketch_singular_values[:k]
-
-    # Compute the ratio with the corresponding bound
-    ratios = sigma_Y / sigma_X
-    lower_bound = np.sqrt(1 - epsilon)
-    upper_bound = np.sqrt(1 + epsilon)
+    ratios, lower_bound, upper_bound = compute_ratio_and_bounds(N, n, k, m, epsilon)
 
     # Draw the diagram
     draw_diagram3(range(1, k+1), 
@@ -48,19 +53,7 @@ def test_value_ratio_n_JL(N = 500, n = 20, k = 10, epsilon = 0.1, m = 40):
     >>> epsilon = 0.1       # Error
     >>> m = 40              # Compressed rows number
     """
-
-    # Generate the experiment data
-    X, true_singular_values, _, _ = generate_matrix_with_singular_values(N, n, sigma=np.linspace(10, 1, k))
-    sketch_singular_values, _ = sketched_svd(X, m)
-
-    # Take the first k terms
-    sigma_X = true_singular_values[:k]
-    sigma_Y = sketch_singular_values[:k]
-
-    # Compute the ratio with the corresponding bound
-    ratios = sigma_Y / sigma_X
-    lower_bound = np.sqrt(1 - epsilon)
-    upper_bound = np.sqrt(1 + epsilon)
+    ratios, lower_bound, upper_bound = compute_ratio_and_bounds(N, n, k, m, epsilon)
 
     # Draw the diagram
     draw_diagram3(
